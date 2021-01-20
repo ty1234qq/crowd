8e21b8880132376360b024adee8e961f05407c81
tianye <442666951@qq.com>
2021-01-20 17:59:20 +0800

    成功连接数据库

---
+++ atcrowdfundingadmin2component/src/main/java/com/atguigu/crowd/funding/mapper/AdminMapper.java
@@ -0,0 +1,33 @@
+package com.atguigu.crowd.funding.mapper;
+
+import com.atguigu.crowd.funding.entity.Admin;
+import com.atguigu.crowd.funding.entity.AdminExample;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AdminMapper {
+    int countByExample(AdminExample example);
+
+    int deleteByExample(AdminExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Admin record);
+
+    int insertSelective(Admin record);
+
+    List<Admin> selectByExample(AdminExample example);
+
+    Admin selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Admin record, @Param("example") AdminExample example);
+
+    int updateByExample(@Param("record") Admin record, @Param("example") AdminExample example);
+
+    int updateByPrimaryKeySelective(Admin record);
+
+    int updateByPrimaryKey(Admin record);
+}
